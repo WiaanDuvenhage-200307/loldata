02bc44c5a8f7c88012eb3a038e864cadd205ca64
WiaanDuvenhage-200307 <200307@virtualwindow.co.za>
2022-03-08 15:54:26 +0200

    Headerjs renamed | Fonts | RRD
    Renamed Header.js to Nav.js. Installed fonts for project. Installed React-router-dom independency.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import './index.css';
+import {Routes, Route} from 'react-router-dom';
+
+import Nav from "./components/Nav";
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
     <div className="App">
-
+      <Nav />
+      <Routes>
+        <Route path='/' element = {<Welcome/>}></Route> //Causes page to display blank
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import './Nav.css';
+
+const Nav = () => {
+    return(
+        <nav>
+            <h1>LolData</h1>
+            <ul>
+                <li>Dashboard</li>
+                <li>Compare</li>
+                <li>Timeline</li>
+                
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav;
