f2d5e119b72ebb3126d7ec310302a5b6a5920b24
WiaanDuvenhage-200307 <200307@virtualwindow.co.za>
2022-03-20 17:43:26 +0200

    Frontend semi-complete
    Implemented frontend and dummy data for compare page

--- src/components/Compare.js
+++ src/components/Compare.js
@@ -1,5 +1,7 @@
+import React from 'react';
 import './Compare.css';
 import axios from 'axios';
+import "https://kit.fontawesome.com/3d7d8906d0.js";
 
 // ! Champions API Call
 axios.get('https://api.sportsdata.io/v3/lol/stats/json/Champions?key=94c287b249d74701adf60e03aa398884')
@@ -24,17 +26,73 @@ const Compare = () => {
             </div>
 
             <div className='intro'>
-                <h2>COMPARE</h2>
+                <h1>COMPARE</h1>
                 <br/>
-                <p>Want to know what Champion is the best? Compare them here</p>
+                <h3>Compare Champions with each other. <br/>
+                They will be compared on their <strong>HP</strong>, <strong>Mana</strong>, <strong>Armor</strong>, <strong>Attack</strong> & <strong>Defense</strong>. </h3>
             </div>
+
+            <h2>Choose your champion</h2>
+            <div className='compare-block'>
+
+                {/* Would use this for compare page! */}
+                <div className="exCon">
+                    <h4>The Champion has selected: Champion</h4>
+                    <select>
+                        <option>Annie</option>
+                        <option>Aatrox</option>
+                        <option>Alistar</option>
+                    </select>
+                </div>
+
+            </div>
+
+            <div className='compare-block'>
+
+
+                {/* Would use this for compare page! */}
+                <div className="exCon">
+                    <h4>The Champion has selected: Champion</h4>
+                    <select>
+                        <option>Option One</option>
+                        <option>Option Two</option>
+                        <option>Option Three</option>
+                    </select>
+                </div>
+
+                
+
+            </div>
+
+            <div className='result'>
+
+                <div className='output'> //! Champions from dropdown will be displayed here
+                    <h3>Champion Name</h3>
+                    <img src='https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Annie_0.jpg'/>
+                    <i class="fa-solid fa-heart"></i><p>HP</p>
+                    <i class="fa-solid fa-khanda"></i><p>Attack</p>
+                    <i class="fa-solid fa-shield-halved"></i><p>Defense</p>
+                    <i class="fa-brands fa-audible"></i><p>Attack Range</p>
+                    <i class="fa-solid fa-shield"></i><p>Armor</p>
+                </div>
+
+                <h2>VS</h2>
+
+                <div className='output'> //! 
+                    <h3>Champion 2</h3>
+                    <img src='https://lolstatic-a.akamaihd.net/frontpage/apps/prod/rg-champion-aatrox/en_GB/5b922bef08881410f8fffa7273c30a75dfb1d11f/assets/downloads/wallpapers/aatrox-1920x1080.jpg'/>
+                    <i class="fa-solid fa-heart"></i><p>HP</p>
+                    <i class="fa-solid fa-khanda"></i><p>Attack</p>
+                    <i class="fa-solid fa-shield-halved"></i><p>Defense</p>
+                    <i class="fa-brands fa-audible"></i><p>Attack Range</p>
+                    <i class="fa-solid fa-shield"></i><p>Armor</p>
+                </div>
+
+            </div>
+
+           
 
             
-            <div className='graph-block'>
-
-            <h2>Block</h2>
-
-            </div>
 
         </>
     )
