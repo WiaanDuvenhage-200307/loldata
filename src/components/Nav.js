c2d9cee7cdeb3c7607ed01c5d1cd5d3903c0ccbd
WiaanDuvenhage-200307 <200307@virtualwindow.co.za>
2022-03-22 14:58:10 +0200

    Added {Link} to project
    Added Link dependency and replaced it with <a> tag

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,15 @@
 import './Nav.css';
 import "https://kit.fontawesome.com/3d7d8906d0.js";
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
     return(
         <nav>
             <h1>LOLDATA</h1>
             <ul>
-                <a href='/'><li className='dashboard'><i class="fa-solid fa-gauge"></i>Dashboard</li></a>
-                <a href='/Compare'><li className='compare'><i class="fa-solid fa-scale-balanced"></i>Compare</li></a>
-                <a href='/Timeline'><li className='timeline'><i class="fa-solid fa-timeline"></i>Timeline</li></a>
+                <Link to="/"><li className='dashboard'><i class="fa-solid fa-gauge"></i>Dashboard</li></Link>
+                <Link to="Compare"><li className='compare'><i class="fa-solid fa-scale-balanced"></i>Compare</li></Link>
+                <Link to="Timeline"><li className='timeline'><i class="fa-solid fa-timeline"></i>Timeline</li></Link>
                 
             </ul>
         </nav>
