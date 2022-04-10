b62774e55fd89c5c71f140a41890103f1e1102d8
WiaanDuvenhage-200307 <200307@virtualwindow.co.za>
2022-04-11 00:09:24 +0200

    Completed Timeline

--- src/components/charts/TimeChart.js
+++ src/components/charts/TimeChart.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 import './TimeChart.css'
 import {useState, useEffect, useRef} from 'react';
 import 'chartjs-adapter-date-fns';
+import { set } from "date-fns";
 
 // TODO: Get teams from all over the world and append dynamically to dropdown.
 // TODO: When clicking on team from dropdown run function to populate chart with its players.
@@ -14,9 +15,22 @@ import 'chartjs-adapter-date-fns';
 // https://api.sportsdata.io/v3/lol/scores/json/ActiveMemberships?key=94c287b249d74701adf60e03aa398884 TO GET TEAMS
 
 const TimeChart = () =>{
+    const [teams, setTeams] = useState([]);
+
+    const [selectedTeamMates, setSelectedTeamMates] = useState([]);
+
+    const [playtime, setPlayTime] = useState([]);
+
+    const[endtime, setEndTime] = useState([]);
+
+    const[startAndEnd, setStartAndEnd] = useState([]);
+
+
+
+    let getTeam = useRef();
     
     useEffect(() =>{
-        axios.get("https://api.sportsdata.io/v3/lol/scores/json/ActiveMemberships?key=94c287b249d74701adf60e03aa398884")
+        axios.get("https://api.sportsdata.io/v3/lol/scores/json/Teams?key=94c287b249d74701adf60e03aa398884")
         .then((res) =>{
             let data = res.data;
             console.log(data);
@@ -24,31 +38,82 @@ const TimeChart = () =>{
             // console.log(tournament);
 
             // Get Dates of all tournaments
- 
+            
+           setTeams(res.data);
         })
     },[])
+
+    const teamMates = () => {
+        let selectedTeam = getTeam.current.value;
+        console.log(selectedTeam);
+        let teamId = '';
+        for(let i = 0; i < teams.length; i++ ){
+            if(teams[i].Name === selectedTeam){
+                teamId = teams[i].TeamId;
+            }
+        }
+
+        let newRequest = "https://api.sportsdata.io/v3/lol/scores/json/HistoricalMembershipsByTeam/" + teamId + "?key=94c287b249d74701adf60e03aa398884";
+        console.log(newRequest);
+        axios.get(newRequest)
+        .then((res) =>{
+            console.log(res.data);
+            let data = res.data
+            
+           setSelectedTeamMates(data.map((item)=> item.PlayerName));
+            const chartArray = [];
+           for(let i = 0; data.length; i++){
+               let playTimeSplit = data[i].StartDate.split("T")[0];
+               let playTime = playTimeSplit;
+               let endTimeSplit = data[i].EndDate.split("T")[0];
+               let endTime = endTimeSplit;
+
+               chartArray.push([playTime, endTime]);
+                console.log(chartArray);
+
+                setStartAndEnd(chartArray);
+                
+
+            
+           }
+           console.log(startAndEnd);
+
+           
+
+        //    setPlayTime(data.map((item) => item.StartDate.slice(0, 10)));
+
+        //     console.log(playtime);
+
+        //     setEndTime(data.map((i)=> i.EndDate));
+
+        //     console.log(endtime);
+
+        //     setStartAndEnd([[playtime, endtime]]);
+
+        //     console.log(startAndEnd);
+            
+        })
+
+    }
     return(
         <>
         <div className="drop-down">
-            <select>
-                <option>Please Select...</option>
+            <select onChange={teamMates} ref={getTeam}>
+                {
+                    teams.map(item => <option key={item.TeamId}>{item.Name}</option>)
+                }
             </select>
         </div>
         <div className="timeChart">
             
             <div className="exCon chart">
                 <Bar data={{
-                    labels: ["player one", "player two", "player three", "player four"],
+                    labels: selectedTeamMates,
                     datasets: [{
                         label: ['Player has been Active'],
-                        data: [
-                            ['2022-02-05', '2022-04-25'],
-                            ['2022-01-12', '2022-03-25'],
-                            ['2022-01-06', '2022-04-09'],
-                            ['2022-01-12', '2022-04-02'],
-                            ['2022-05-06', '2022-05-23'],
-                            ['2022-10-15', '2022-11-25']
-                        ],
+                        data: 
+                            startAndEnd
+                        ,
                         backgroundColor: [
                             '#5233FB',
                             '#775FFC',
